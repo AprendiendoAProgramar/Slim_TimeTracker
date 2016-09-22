8b7ac9e07384f15bf321e9603ec3be9fa3ca1578
Miguel Angel <m.zapes@gmail.com>
2016-09-22 17:37:36 +0200

    Add views and templates

--- src/routes.php
+++ src/routes.php
@@ -3,5 +3,6 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    echo 'Hello';
+    $response = $this->view->render($response, "home.php");
+    return $response;
 })->setName('home');
