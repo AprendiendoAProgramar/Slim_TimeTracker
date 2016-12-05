8b0ec07a1733f66f9f008dbc79460a2925dddbeb
Miguel Angel <m.zapes@gmail.com>
2016-12-05 18:26:04 +0100

    Updating autoload files

--- public/index.php
+++ public/index.php
@@ -5,12 +5,6 @@ session_start(); //by default requires session storage
 //require '../vendor/autoload.php';
 require __DIR__ . '/../vendor/autoload.php';
 
-spl_autoload_register(function ($classname) {
-    $file = str_replace('\\', '/', $classname);
-    //require '../' . $file . '.php';
-    require __DIR__ . '/../' . $file . '.php';
-});
-
 // Add config settings and instantiate the App
 $settings = require __DIR__ . '/../app/settings.php';
 $app =  new \Slim\App($settings);
